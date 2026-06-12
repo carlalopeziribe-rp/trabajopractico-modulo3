a0057df509d7733951871dd31b64bf99f21f3aa9
Carla <carlalopeziribe@gmail.com>
2026-06-12 14:53:50 -0300

    Cree las paginas del sitio

---
+++ src/pages/Servicios.jsx
@@ -0,0 +1,7 @@
+function Servicios() {
+  return (
+    <h1>Servicios</h1>
+  )
+}
+
+export default Servicios
---
+++ src/pages/TuRutina.jsx
@@ -0,0 +1,7 @@
+function TuRutina() {
+  return (
+    <h1>Tu Rutina</h1>
+  )
+}
+
+export default TuRutina
