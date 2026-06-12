97fd94084e59644bdcf30c582ce393cb9fd60c1c
Carla <carlalopeziribe@gmail.com>
2026-06-12 14:31:14 -0300

    Cree las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/servicios' element={<Servicios />} />
+                <Route path='/turutina' element={<TuRutina />} />
+                <Route path='/contacto' element={<Contacto />} />
+            </Routes>
+
+        </div>
+    )
 }
 
 export default App
