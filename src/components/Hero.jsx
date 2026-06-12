6f72f623171fa03309fc92cdd58354d0009fb418
Carla <carlalopeziribe@gmail.com>
2026-06-12 15:02:58 -0300

    Cree el componente Hero.jsx y agregue la foto de portada a la carpeta assets

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import portadainicio from '../assets/PortadaInicio.jpg'
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <img
+                            className="foto-portada img-fluid mx-auto d-block"
+                            src={portadainicio}
+                            alt="Difusor de aromas de madera con vapor, acompañado de toallas blancas y elementos de spa sobre una bandeja"
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h1 className="presentacion">MI Centro de Bienestar</h1>
+                        <p className="texto-presentacion">
+                            Un espacio dedicado al cuidado del cuerpo y la mente mediante
+                            masajes y tratamientos faciales y corporales.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
