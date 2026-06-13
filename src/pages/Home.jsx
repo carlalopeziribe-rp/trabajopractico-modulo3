16cd280049c2dda4486ed6d95c9d9a808978c697
Carla <carlalopeziribe@gmail.com>
2026-06-12 21:03:19 -0300

    Corregí un error de escritura

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,9 @@ import NuestrosServicios from "../components/NuestrosServicios"
 
 function Home() {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col">
-          <Hero />
-          <NuestrosServicios />
-        </div>
-      </div>
+    <div>
+      <Hero />
+      <NuestrosServicios />
     </div>
   )
 }
