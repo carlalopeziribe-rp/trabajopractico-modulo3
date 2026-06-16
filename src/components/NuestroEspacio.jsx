95db25947fadebabfc6ef969d9257f7f4cd6b3b8
Carla <carlalopeziribe@gmail.com>
2026-06-16 18:28:28 -0300

    Cree el componente Nuestro Espacio

---
+++ src/components/NuestroEspacio.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import Carrusel1 from '../assets/Carrusel1.jpg'
+import Carrusel2 from '../assets/Carrusel2.jpg'
+import Carrusel3 from '../assets/Carrusel3.jpg'
+import Carrusel4 from '../assets/Carrusel4.jpg'
+import Carrusel5 from '../assets/Carrusel5.jpg'
+
+const imagenesCarrusel = [
+    Carrusel1,
+    Carrusel2,
+    Carrusel3,
+    Carrusel4,
+    Carrusel5
+]
+
+export default function NuestroEspacio() {
+    const [imagenActual, setImagenActual] = useState(0)
+
+    return (
+        <section className='lugar'>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h2 className='presentacion-lugar'>Nuestro Espacio</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="container carrusel-container">
+                <div class="carrusel" id="carrusel">
+                    <div class="carrusel-item">
+                        <img
+                            src={imagenesCarrusel[imagenActual]}
+                            alt="Nuestro espacio" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
