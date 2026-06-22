75f33978e7ed2dc953680acc85820aa3f2c891ee
Carla <carlalopeziribe@gmail.com>
2026-06-22 11:17:36 -0300

    Convertí el logo en un link al inicio y agregué estilos Bootstrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 import logo from '../assets/Logo.png'
 
-export default function NavBar(){
+export default function NavBar() {
     return (
-            <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
-                <img className="navbar-brand" src={logo} alt="logo" width="100"
-                    height="100"/>
+                <Link className="navbar-brand" to="/">
+                    <img
+                        src={logo}
+                        alt="Logo de MI Centro de Bienestar"
+                        width="100"
+                        height="100" />
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div className="navbar-nav gap-4">
                         <Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
                         <Link className="nav-link" to="/servicios">Servicios</Link>
@@ -22,5 +27,5 @@ export default function NavBar(){
                 </div>
             </div>
         </nav>
-       )
+    )
 }
